b3c0258c703ca4dfaeb2ce26b919a73212109004
vara <vara@varavm.ixb55pntrw4ulf50fy0y5cecrf.gx.internal.cloudapp.net>
2019-06-22 14:30:03 +0000

    url pattern change

--- Demo/src/main/java/com/telectronics/rest/GitInfoController.java
+++ Demo/src/main/java/com/telectronics/rest/GitInfoController.java
@@ -2,7 +2,7 @@ package com.telectronics.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.telectronics.service.GitService;
@@ -13,8 +13,8 @@ public class GitInfoController {
 	@Autowired
 	GitService gitService;
 
-	@GetMapping(value = "/projects", params = "username")
-	public String getProjectsList(@RequestParam(name = "username") String userName) {
+	@GetMapping(value = "/projects/{username}")
+	public String getProjectsList(@PathVariable("username") String userName) {
 		int count = gitService.getGitInfoByName(userName);
 		String str="<h1>Number of Projects : "+count+"<h1>";
 		return str;
